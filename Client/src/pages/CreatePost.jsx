71be78661b277da209d8f8e39a557fc4363a53d4
talhaunas1 <mtalha4600@gmail.com>
2023-05-04 23:42:15 +0500

    working on form field

--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utilis'
+import { FormField, Loader } from '../components'
+
 
 const CreatePost = () => {
+  //this allow us to negative hoem once the post is created
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: ''
+  })
+
+  const [generatingImg, setGeneratingImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+
+const handleSubmit=()=>{
+
+}
+
+const handleChange=(e)=>{
+
+}
+const handleSurpriseMe=()=>{
+
+}
+
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div >
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+          Create imaginative and visually stunning images through by the DALL-E AI and share them with the community.
+        </p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            lableName=" Your Nmae"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+            />
+          <FormField 
+            lableName=" Prompt"
+            type="text" 
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurprisedMe
+            handleSurpriseMe={handleSurpriseMe}
+            />
+        </div>
+      </form>
+    </section>
   )
 }
 
